6c7463094ebbdc4f501c00d6098692f0f6b3e503
SebastienFeser <sedukafe@gmail.com>
2024-09-17 20:38:23 +0200

    added sprites & simple UI

---
+++ Assets/Scripts/DayCycle.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayCycle : MonoBehaviour
+{
+    public enum Day
+    {
+        h9,
+        h12,
+        h15,
+        h18,
+        h21,
+    }
+        
+}
---
+++ Assets/Scripts/PlayerResources.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerResources : MonoBehaviour
+{
+    private int mentalHealth;
+    private int physicalCondition;
+    private int motivation;
+    private int money;
+    private int societalIntegration;
+    private int social;
+}
